19af1e6101d4a93413fc783e396149bfd9512457
Michael Spaxman <seanforyou23@gmail.com>
2019-01-30 21:02:14 -0500

    [issue-1253] add alert/alertgroup test page

---
+++ js/alert-group.js
@@ -0,0 +1,118 @@
+(function ($) {
+  'use strict';
+
+  const alert1 = () => `
+  <div class="pf-c-alert pf-m-success" aria-label="Success Alert" id="alert1">
+    <div class="pf-c-alert__icon">
+      <i class="fas fa-check-circle" aria-hidden="true"></i>
+    </div>
+    <h4 class="pf-c-alert__title">
+      <span class="pf-screen-reader">Success alert:</span>
+      Sucess loading the Alert and AlertGroup acessibility test page
+    </h4>
+    <div class="pf-c-alert__description">
+      <p>This test page shows an ideal combination of markup attributes for announcing changes to a relevant part of a
+        webpage.
+        <a href="https://www.w3.org/TR/wai-aria-1.1/#aria-live">Read this Property Definition</a>
+      </p>
+    </div>
+    <div class="pf-c-alert__action">
+      <button class="pf-c-button close-btn pf-m-plain" aria-label="Close Success Alert: Loaded alert test page">
+        <i class="fas fa-times" aria-hidden="true"></i>
+      </button>
+    </div>
+  </div>
+  `;
+
+  const alert2 = () => `
+  <div class="pf-c-alert pf-m-info" aria-label="Information Alert" id="alert2">
+    <div class="pf-c-alert__icon">
+      <i class="fas fa-info-circle" aria-hidden="true"></i>
+    </div>
+    <h4 class="pf-c-alert__title">
+      <span class="pf-screen-reader">Info alert:</span>
+      Set both aria-live and role attributes
+    </h4>
+    <div class="pf-c-alert__description">
+      <p>
+        While the aria-live attribute is the <em>primary</em> determination for how live regions are announced, developers should also include any corresponding <a href="https://www.w3.org/TR/wai-aria-1.1/#live_region_roles">live region role</a> for <a href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions#Preferring_Specialized_Live_Region_Roles">compatibility reasons</a>.
+      </p>
+    </div>
+    <div class="pf-c-alert__action">
+      <button class="pf-c-button close-btn pf-m-plain" aria-label="Close Info Alert: Set aria-live and role attributes">
+        <i class="fas fa-times" aria-hidden="true"></i>
+      </button>
+    </div>
+  </div>
+`;
+
+  const alert3 = () => `
+  <div class="pf-c-alert pf-m-danger" aria-label="Error Alert" id="alert3">
+    <div class="pf-c-alert__icon">
+      <i class="fas fa-exclamation-circle" aria-hidden="true"></i>
+    </div>
+    <h4 class="pf-c-alert__title">
+      <span class="pf-screen-reader">Error alert:</span>
+      There was a problem submitting your form
+    </h4>
+    <div class="pf-c-alert__description">
+      <p>Please enter your first name.</p>
+    </div>
+    <div class="pf-c-alert__action">
+      <button class="pf-c-button close-btn pf-m-plain" aria-label="Close Error Alert: Problem submitting your form">
+        <i class="fas fa-times" aria-hidden="true"></i>
+      </button>
+    </div>
+  </div>
+  `;
+
+  const autoDismissTime = (ms) => {
+    return ms + 8000;
+  };
+
+  document.addEventListener("DOMContentLoaded", function (event) {
+    const $alertContainer = $('#alert-container');
+    const $sampleForm = $('#sample-form');
+    const alert1Start = 5000;
+    const alert2Start = alert1Start + 3000;
+    const alert1End = autoDismissTime(alert1Start);
+    const alert2End = autoDismissTime(alert2Start);
+    function startAlertTimers() {
+      // automatically insert Alert 1 ~5 seconds after page load
+      setTimeout(() => {
+        $alertContainer.append(alert1());
+      }, alert1Start);
+      // automatically insert Alert 2 ~3 seconds after Alert 1
+      setTimeout(() => {
+        $alertContainer.append(alert2());
+      }, alert2Start);
+      // automatically dismiss each alert after ~8 seconds
+      setTimeout(() => {
+        $alertContainer.find('#alert1').remove();
+      }, alert1End);
+      setTimeout(() => {
+        $alertContainer.find('#alert2').remove();
+      }, alert2End);
+    };
+
+    startAlertTimers();
+
+    $sampleForm.on('submit', event => {
+      event.preventDefault();
+      const invalid = $sampleForm.find('input').val() === '';
+      // on submission, an alert displays
+      if (invalid) {
+        $alertContainer.append(alert3());
+        // After the form alert displays, repeat steps 1-3
+        startAlertTimers();
+      }
+    });
+
+    // any alert can be manually dismissed
+    $(document).on('click', '.close-btn', function () {
+      $(this).parents('.pf-c-alert').remove().blur();
+    });
+
+  });
+
+})(jQuery);
